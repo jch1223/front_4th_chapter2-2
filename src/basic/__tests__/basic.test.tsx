f3a53a4fd4b829f1bcbb22b93b0405dadec4647e
jch1223 <ico1828@gmail.com>
2025-01-14 07:19:08 +0900

    🏗️ Product 관련 로직을 entity폴더로 이동

--- src/basic/__tests__/basic.test.tsx
+++ src/basic/__tests__/basic.test.tsx
@@ -5,7 +5,7 @@ import { describe, expect, test } from 'vitest';
 import { useCart } from '@/refactoring/entity/cart/hooks/useCart';
 import * as cartUtils from '@/refactoring/entity/cart/utils/cart';
 import { useCoupons } from '@/refactoring/entity/coupon/hooks/useCoupon';
-import { useProducts } from '@/refactoring/hooks';
+import { useProducts } from '@/refactoring/entity/product/hooks/useProduct';
 import { AdminPage } from '@/refactoring/pages/AdminPage';
 import { CartPage } from '@/refactoring/pages/CartPage';
 import type { CartItem, Coupon, Product } from '@/types';
--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { INITIAL_COUPONS, useCoupons } from '@/refactoring/entity/coupon/hooks/useCoupon';
-import { INITIAL_PRODUCTS, useProducts } from '@/refactoring/hooks';
+import { INITIAL_PRODUCTS, useProducts } from '@/refactoring/entity/product/hooks/useProduct';
 import { AdminPage } from '@/refactoring/pages/AdminPage';
 import { CartPage } from '@/refactoring/pages/CartPage';
 
